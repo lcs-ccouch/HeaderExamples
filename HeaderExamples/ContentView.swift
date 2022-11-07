4e033fbef2e27505d4076b61528b411d25214b64
Calum Couch <ccouch@lcs.on.ca>
2022-11-07 12:20:23 -0500

    working name badge

--- HeaderExamples/ContentView.swift
+++ HeaderExamples/ContentView.swift
@@ -9,24 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+       
+        
         VStack {
-            Text("Calum") // Create text on the screen
-                .font(.largeTitle) // Use a font modifier to make text larger
             
+            Text("Name:Calum Couch") // Create text on the screen
+                .padding()
+                .background(Color.white)
+                .foregroundColor(Color.black
+                )
+                .cornerRadius(5) // Rounded corners on whole text view
             
             VStack(spacing: 20) { // VStack is a container view that can hold up to 10 views
                 
-                Text("Hockey")
+                Text("Hockey/Student")
                     .font(.title) // Set to be the second largest font.
-                    .foregroundColor(Color.gray) // Change text color to
-                
+                    .foregroundColor(Color.black) // Change text color to
+                    .background(Color.white)
+                    .cornerRadius(5) // Rounded corners on whole text view
+                    
             }
             .padding()
+            
+        }
+        .background(Color.black)
+        .cornerRadius(20) // Rounded corners on whole text view
         }
     }
     
     
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
