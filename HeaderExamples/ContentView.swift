1dcb9afeb0930c7bac394ae348a702228e05ee92
Calum Couch <ccouch@lcs.on.ca>
2022-11-07 11:44:36 -0500

    Working title and subtitle with stack

--- HeaderExamples/ContentView.swift
+++ HeaderExamples/ContentView.swift
@@ -10,13 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Calum") // Create text on the screen
+                .font(.largeTitle) // Use a font modifier to make text larger
+            
+            
+            VStack(spacing: 20) { // VStack is a container view that can hold up to 10 views
+                
+                Text("Hockey")
+                    .font(.title) // Set to be the second largest font.
+                    .foregroundColor(Color.gray) // Change text color to
+                
+            }
+            .padding()
         }
-        .padding()
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
