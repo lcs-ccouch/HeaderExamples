2ed971b08b77663a6be7737beed00f10fa9c0c8b
Calum Couch <ccouch@lcs.on.ca>
2022-11-07 12:25:32 -0500

    new working name badge with good outline background

--- HeaderExamples/ContentView.swift
+++ HeaderExamples/ContentView.swift
@@ -9,17 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-       
+        
         
         VStack {
             
-            Text("Name:Calum Couch") // Create text on the screen
-                .padding()
-                .background(Color.white)
-                .foregroundColor(Color.black
-                )
-                .cornerRadius(5) // Rounded corners on whole text view
+            Spacer()
+                .frame(maxHeight: 30)
             
+            Text("Name:Calum Couch") // Create text on the screen
+                .font(.title)
+                .background(Color.white)
+                .foregroundColor(Color.black)
+                .cornerRadius(5) // Rounded corners on whole text view
+                .frame(maxWidth: 350)
             VStack(spacing: 20) { // VStack is a container view that can hold up to 10 views
                 
                 Text("Hockey/Student")
@@ -27,17 +29,17 @@ struct ContentView: View {
                     .foregroundColor(Color.black) // Change text color to
                     .background(Color.white)
                     .cornerRadius(5) // Rounded corners on whole text view
-                    
+                
             }
             .padding()
             
         }
         .background(Color.black)
         .cornerRadius(20) // Rounded corners on whole text view
-        }
     }
-    
-    
+}
+
+
 
 
 struct ContentView_Previews: PreviewProvider {
