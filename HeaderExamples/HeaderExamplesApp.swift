4e034ab3d52b91ec7bbbffb87bf41d409fb90eaf
Calum Couch <ccouch@lcs.on.ca>
2022-11-07 11:24:38 -0500

    Initial Commit

---
+++ HeaderExamples/HeaderExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderExamplesApp.swift
+//  HeaderExamples
+//
+//  Created by calum couch on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct HeaderExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
